63e200ccb26fcaf8f40e41d296ba33ac8f301e6d
nicolas rouvet <nico.rouvet@gmail.com>
2020-04-12 18:53:23 -0400

    function Random numpy

--- main.py
+++ main.py
@@ -5,4 +5,30 @@ Created on Sun Apr 12 10:37:21 2020
 @author: Nicolas Rouvet
 """
 
-print("Hello World")
+import numpy as np
+
+
+#création des différentes fonction aléatoire nécessaire
+
+#
+f=4
+
+
+
+def Arrivee():
+    arrivee = np.random.poisson(0.5)
+    return arrivee
+
+def ChoixEtage(f):
+    choixEtage = round(np.random.uniform(1,f))
+    return choixEtage
+
+def tempsTravail():
+    tempTravail = np.random.exponential(60)
+    return tempsTravail
+
+
+
+
+
+
