fefa0b990837df633bd2dae58ad93e0aae996411
nicolas rouvet <nico.rouvet@gmail.com>
2020-04-18 12:32:41 -0400

    Fonction Deroulement Journée

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ Created on Sun Apr 12 10:37:21 2020
 """
 
 import numpy as np
-
+from pylab import plot
 
 #création des différentes fonction aléatoire nécessaire
 
 #
 f=4
-
-
 
 def Arrivee():
     arrivee = np.random.poisson(0.5)
@@ -24,9 +22,27 @@ def ChoixEtage(f):
     return choixEtage
 
 def tempsTravail():
-    tempTravail = np.random.exponential(60)
+    tempsTravail = np.random.exponential(60)
     return tempsTravail
 
+
+def deroulementJournee(max):
+    t = max*60 #convertir les heures en minutes
+    personne =[0]*t
+    i=0
+    while(i<t):
+        personne[i] = Arrivee()
+        i+=1
+    x = range(t)
+    y = personne
+    plot(x, y)
+    return personne
+
+
+
+print(deroulementJournee(5))
+
+        
 
 
 
