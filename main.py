85f8c4eb9c36e2b88b4e87548bef83398bd6e82e
emylz <eminyalaz@gmail.com>
2020-04-24 18:43:02 +0200

    ascenseur

--- main.py
+++ main.py
@@ -19,9 +19,11 @@ class Ascenseur:
         self.capacite = []    #personnes dans l'ascenseur
         
     def arriveePersonne(self, personne):
-        eta = personne.etage
         if(0 not in self.etages and self.current != 0):
             self.etages.append(0)
+            
+    def entreePersonne(self, personne):
+        eta = personne.etage
         if(eta not in self.etages):
             self.etages.append(eta)
         self.capacite.append(personne)
@@ -84,6 +86,11 @@ class Ascenseur:
                 c.attente = temps - c.arrivee  #temps d'attente en seconde
                 tempsAttente.append(c.attente)
                 self.capacite.remove(c)
+                if(self.current != 0):
+                    personnes.append(c)
+                    i = personnes.index(c)
+                    personnes[i].etageAppel = personnes[i].etage
+                    personnes[i].etage = 0
         
     
     
@@ -99,6 +106,7 @@ class Personne:
         self.depart = depart    #temps où la personne part de l'immeuble (en minute)
         self.arrivee = arrivee    #temps d'arrivée de la personne  (en seconde)
         self.attente = 0        #utilisé pour calculé le temps d'attente de la personne (en seconde)
+        self.etageAppel = 0
         
 f=4
 
@@ -131,6 +139,7 @@ def main():
     sec = 0  #Compteur secondes
     minute = 0  #Compteur minutes
     a = Ascenseur()
+    tmp = []
     while(sec<200):  #18000s = 5h
         if(sec%60==0):
             nb = Arrivee()
@@ -139,17 +148,16 @@ def main():
                 etage = ChoixEtage(f)
                 temps = tempsTravail() + minute
                 pers = Personne(etage, temps, sec)
-                personnes.append(pers)
+                tmp.append(pers)
                 a.arriveePersonne(pers)
                 i+=1
                    
             for p in personnes:
                 if(p.depart == minute):
-                    etage = p.etage
+                    etage = p.etageAppel
                     if(etage not in a.etages):
                         a.etages.append(p.etage)
-                    if(0 not in a.etages):
-                        a.etages.append(0)
+                    tmp.append(p)
                     personnes.remove(p)
                     p.arrivee = sec
             minute+=1 
@@ -162,6 +170,9 @@ def main():
         
         if(a.disponibilite == True):
             a.sortieAcenseur(sec)
+            for t in tmp:
+                if(t.etageAppel == a.current):
+                    a.entreePersonne(t)
         
         sec+=1
     
